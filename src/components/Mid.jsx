c33bed9084cae89af7a1d20967a2b2c551af7aff
maheendran kp <maheendrankp100@gmail.com>
2022-05-19 20:56:18 +0530

    navbar added

---
+++ src/components/Mid.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Mid =() =>{
+    return  <div className='middiv'>
+    <p>Service</p>
+    <p>Project</p>
+    <p>About</p>
+  </div>
+}
+export default Mid;
---
+++ src/components/Right.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const Right =() =>{
+    return <div>
+       <button className='btn'>contact</button>
+    </div>
+}
+export default Right;
